f112303a1b7b1dff2d5ad4382956fd04641e800b
Walid31 <ghalemwalid31@gmail.com>
2019-11-06 13:31:38 +0100

    creating the view of the Todo app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('view engine', 'ejs');
 
 // static files
-app.use('/assets', express.static('./public'));
+app.use(express.static('./public'));
 
 //fire controllers
 todoController(app);
