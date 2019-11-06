c7f87bfbfc0316d60aa1ab591b40604fc0c41f69
Walid31 <ghalemwalid31@gmail.com>
2019-11-06 15:31:44 +0100

    now we can add and delete items from the Todo list

--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,6 +1,37 @@
 $(document).ready(function(){
 
-  $('form').on('submit', function(){
+  console.log('sending');
+  
+   $("#submit").on("click",function () {
+    console.log('click event');
+
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+      console.log('todo obj',todo);
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){       
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+     
+     
+   })
+
+  $('form').on('submit', function(e){
+
+    console.log('submitting');
+    e.preventDefault()
+
+
+    
 
       var item = $('form input');
       var todo = {item: item.val()};
@@ -10,6 +41,8 @@ $(document).ready(function(){
         url: '/todo',
         data: todo,
         success: function(data){
+          console.log(data);
+          
           //do something with the data via front-end framework
           location.reload();
         }
@@ -20,13 +53,14 @@ $(document).ready(function(){
   });
 
   $('li').on('click', function(){
+    
       var item = $(this).text().replace(/ /g, "-");
       $.ajax({
         type: 'DELETE',
         url: '/todo/' + item,
         success: function(data){
           //do something with the data via front-end framework
-          location.reload();
+         location.reload();
         }
       });
   });
