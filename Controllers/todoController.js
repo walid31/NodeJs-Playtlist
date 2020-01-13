32182cc4c885e1647182d4a27e0701f8ea041356
Walid31 <ghalemwalid31@gmail.com>
2020-01-13 02:11:19 +0100

    the final commit

--- Controllers/todoController.js
+++ Controllers/todoController.js
@@ -1,6 +1,31 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+//connect to the database
+//mongoose.connect('mongodb+srv://test:test@cluster0-vrst1.mongodb.net/test?retryWrites=true&w=majority');
+
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb+srv://test:test@cluster0-vrst1.mongodb.net/test?retryWrites=true&w=majority";
+var client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  var collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+//create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err) {
+    if (err) throw err;
+    console.log('item saved');
+    
+});
+
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
